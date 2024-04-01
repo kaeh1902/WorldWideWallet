268824f5158f3b082d828212584d50220529b126
yuliiapiguliakCU <143627409+yuliiapiguliakCU@users.noreply.github.com>
2024-04-01 13:54:07 -0600

    fixeddb (#27)

--- ProjectSourceCode/src/init_data/create.sql
+++ ProjectSourceCode/src/init_data/create.sql
@@ -3,13 +3,13 @@ CREATE TABLE IF NOT EXISTS users (
     user_id SERIAL PRIMARY KEY NOT NULL,
     name VARCHAR(100),
     username VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    password VARCHAR(20) NOT NULL
+    email VARCHAR(100),
+    password CHAR(60) NOT NULL
 );
 
 DROP TABLE IF EXISTS conversions CASCADE;
 CREATE TABLE IF NOT EXISTS conversions (
-    conversion_id SERIAL PRIMARY KEY NOT NULL,
+    conversion_id SERIAL PRIMARY KEY NOT NULL
 ); 
 
 DROP TABLE IF EXISTS users_to_conversions CASCADE;
