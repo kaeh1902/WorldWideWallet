f817ea865669f780d6838e517504b3b52b5f1165
Daniel Folino <dafo2334@colorado.edu>
2024-04-01 12:06:23 -0600

    connection to database and partial implementation of pages

--- ProjectSourceCode/src/index.js
+++ ProjectSourceCode/src/index.js
@@ -267,3 +267,157 @@ function clearFields() {
     document.getElementById('exchangeRate').textContent = '-';
     document.getElementById('conversionText').textContent = 'Euro (EUR) to U.S Dollar (USD)';
 }
+
+//-------------------------------------------------------------------------------------------------
+
+const express = require('express'); // To build an application server or API
+const app = express();
+const handlebars = require('express-handlebars');
+const Handlebars = require('handlebars');
+const path = require('path');
+const pgp = require('pg-promise')(); // To connect to the Postgres DB from the node server
+const bodyParser = require('body-parser');
+const session = require('express-session'); // To set the session object. To store or access session data, use the `req.session`, which is (generally) serialized as JSON by the store.
+const bcrypt = require('bcrypt'); //  To hash passwords
+const axios = require('axios'); // To make HTTP requests from our server. We'll learn more about it in Part C.
+
+
+
+// create `ExpressHandlebars` instance and configure the layouts and partials dir.
+const hbs = handlebars.create({
+  extname: 'hbs',
+  layoutsDir: __dirname + '/views/layouts',
+  partialsDir: __dirname + '/views/partials',
+});
+
+// database configuration
+const dbConfig = {
+  host: 'db', // the database server
+  port: 5432, // the database port
+  database: process.env.POSTGRES_DB, // the database name
+  user: process.env.POSTGRES_USER, // the user account to connect with
+  password: process.env.POSTGRES_PASSWORD, // the password of the user account
+};
+
+const db = pgp(dbConfig);
+
+// test your database
+db.connect()
+  .then(obj => {
+    console.log('Database connection successful'); // you can view this message in the docker compose logs
+    obj.done(); // success, release the connection;
+  })
+  .catch(error => {
+    console.log('ERROR:', error.message || error);
+  });
+
+// *****************************************************
+// <!-- Section 3 : App Settings -->
+// *****************************************************
+
+// Register `hbs` as our view engine using its bound `engine()` function.
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(bodyParser.json()); // specify the usage of JSON for parsing request body.
+
+// initialize session variables
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    saveUninitialized: false,
+    resave: false,
+  })
+);
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+
+app.get('/', (req, res) => {
+    res.redirect('/login');
+  });
+
+app.get('/login', (req, res) => {
+    res.render('pages/login');
+});
+
+app.get('/register', (req, res) => {
+    res.render('pages/register');
+});
+
+
+app.post('/register', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    
+    await db.none('INSERT INTO users(username, password) VALUES($1, $2)', [username, hashedPassword]);
+    res.redirect('/login');
+
+  } 
+  catch (error) {
+    console.error('Error registering user:', error);
+    res.redirect('/register');
+  }
+});
+
+app.post('/login', async (req, res) => {
+  try {
+
+    const user = await db.oneOrNone('SELECT * FROM users WHERE username = $1', req.body.username);
+    
+    if (!user) {
+      res.redirect('/register');
+      return;
+    }
+    
+    const match = await bcrypt.compare(req.body.password, user.password);
+    
+    if (match) {
+      req.session.user = user;
+      req.session.save();
+      
+      res.redirect('/discover');
+    } else {
+      res.render('pages/login', { message: 'Incorrect username or password.' });
+    }
+  } catch (error) {
+    console.error('Error:', error);
+    res.render('pages/login', { message: 'An error occurred. Please try again.' });
+  }
+});
+
+// Authentication Middleware.
+const auth = (req, res, next) => {
+  if (!req.session.user) {
+    // Default to login page.
+    return res.redirect('/login');
+  }
+  next();
+};
+
+// Authentication Required
+app.use(auth);
+
+app.get('/logout', (req, res) => {
+    req.session.destroy(err => {
+      if (err) {
+        console.error('Error destroying session:', err);
+        return res.redirect('/'); 
+      }
+      res.clearCookie('connect.sid');
+      res.render('pages/logout')
+    });
+  });
+  
+  
+  // *****************************************************
+  // <!-- Section 5 : Start Server-->
+  // *****************************************************
+  // starting the server and keeping the connection open to listen for more requests
+  app.listen(3000);
+  console.log('Server is listening on port 3000');
