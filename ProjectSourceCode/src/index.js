4c0ded549b77aafcbae30948cd00e5ad478e21b3
Daniel Folino <dafo2334@colorado.edu>
2024-04-08 18:20:10 -0600

    removed console.log that was used for debugging

--- ProjectSourceCode/src/index.js
+++ ProjectSourceCode/src/index.js
@@ -137,7 +137,6 @@ app.post('/login', async (req, res) => {
 try {
 
   const user = await db.oneOrNone('SELECT * FROM users WHERE username = $1', req.body.username); // compare login credetials to those ones that exist
-  console.log(user);
 
   if (!user) { // if user dose not match any exising users redirect to register page so they can register 
     res.redirect('/register');
